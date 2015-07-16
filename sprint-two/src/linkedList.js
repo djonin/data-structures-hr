5da797ca4b9ce728aa24ba3ce33224389fb1c19f
Matt Schaeffer <matt.schaeffer@autodesk.com>
2015-07-16 10:28:34 -0700

    Submitting linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+
+    if (list.tail){
+      list.tail.next = newNode;
+    }
+
+    if (!list.head){
+      list.head = newNode;
+    }
+
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var removedHead = list.head;
+
+    if (list.head){
+      list.head = list.head.next;
+    }
+
+    if(!list.head){
+      list.tail = null;
+    }
+
+    return removedHead.value;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+
+    while(currentNode !== null){
+      if (target === currentNode.value){
+        return true;
+      }else{
+        currentNode = currentNode.next;
+      }
+    }
+
+    return false;
   };
 
   return list;
