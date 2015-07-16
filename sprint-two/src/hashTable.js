b475cd6d650e9ef94a08ab3be8c5d12cb91b8ff3
Dmitrijs Jonins <randomnickname1@gmail.com>
2015-07-16 13:43:31 -0700

    solved hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,28 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+ 	var tempVar = this._storage.get(i);
+  if(tempVar == null) {
+  	tempVar = {};
+  }
+
+  tempVar[k] = v;
+  this._storage.set(i, tempVar);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  if (this._storage.get(i)[k] === undefined)
+  	return null;
+  return this._storage.get(i)[k];
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var tempVar = this._storage.get(i);
+  delete tempVar[k];
+  this._storage.set(i, tempVar);
 };
 
 
