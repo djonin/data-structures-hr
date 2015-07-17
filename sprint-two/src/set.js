dddc88e981a543b399aea39e86b7cf6f9b2d2a6f
Dmitrijs Jonins <randomnickname1@gmail.com>
2015-07-16 23:35:39 -0700

    set for objects

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,3 +1,31 @@
+//stable stringify
+var stringifyJSON = function(obj) {
+  if (typeof obj === "string") {
+  	return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+  	var resultArr = [];
+  	for (var i = 0; i < obj.length; i++) {
+  		resultArr.push(stringifyJSON(obj[i]));
+  	}
+  	return "[" + resultArr.join(',') + "]";
+  } else if (obj && typeof obj === "object") {
+  	var resultObj = [];
+  	var keysArr = Object.keys(obj);
+  	//sort the keys so method is stable
+  	keysArr = keysArr.sort();
+  	for (var i = 0; i<keysArr.length; i++) {
+  		var key = keysArr[i];
+  		if (obj[key] !== undefined && typeof obj[key] !== "function") {
+  			resultObj.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+  		}
+  	}
+  	return "{" + resultObj.join(',') + "}";
+  } else if (typeof obj === "function") {
+  	return undefined;
+  }
+  return "" + obj;
+};
+
 var Set = function(){
   var set = Object.create(setPrototype);
   set.storage = {};
@@ -7,15 +35,15 @@ var Set = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
-	this.storage[JSON.stringify(item)] = item;
+	this.storage[stringifyJSON(item)] = item;
 };
 
 setPrototype.contains = function(item){
-	return this.storage[JSON.stringify(item)] !== undefined;
+	return this.storage[stringifyJSON(item)] !== undefined;
 };
 
 setPrototype.remove = function(item){
-	delete this.storage[JSON.stringify(item)];
+	delete this.storage[stringifyJSON(item)];
 };
 
 /*
