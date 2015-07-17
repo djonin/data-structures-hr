4ed5ce213217142701c58e0cca8829618e96b80f
Dmitrijs Jonins <randomnickname1@gmail.com>
2015-07-17 01:56:55 -0700

    WIP on bst rebalance

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,9 +1,14 @@
+
 var BinarySearchTree = function(value){
 	var tree = Object.create(BinarySearchTree.prototype);
 
 	tree.value = value;
 	tree.left = null;
 	tree.right = null;
+
+	tree.minDepth = 0;
+
+	tree.maxDepth = 0;
 
 	return tree;
 };
@@ -32,19 +37,63 @@ BinarySearchTree.prototype.breadthFirstLog = function(callback) {
 }
 
 BinarySearchTree.prototype.insert = function(value){
-	if (value > this.value){
-		if (!this.right){
-			this.right = BinarySearchTree(value);
-		}else{
-			this.right.insert(value);
+
+	var recursiveInsert = function(tree) {
+		var newDepth = 0;
+		if (value > tree.value){
+			if (!tree.right){
+				tree.right = BinarySearchTree(value);
+				newDepth = 1;
+			} else {
+				newDepth = recursiveInsert(tree.right);
+			}
+		} else {
+			if (!tree.left){
+				tree.left = BinarySearchTree(value);
+				newDepth = 1;
+			}else{
+				newDepth = recursiveInsert(tree.left);
+			}
 		}
-	}else{
-		if (!this.left){
-			this.left = BinarySearchTree(value);
-		}else{
-			this.left.insert(value);
+		if(tree.left && tree.right) {
+			tree.minDepth = tree.left.minDepth < tree.right.minDepth ? tree.left.minDepth+1 : tree.right.minDepth+1;
 		}
+		if(newDepth > tree.maxDepth) {
+			tree.maxDepth = newDepth;
+		}
+		return newDepth+1;
 	}
+	recursiveInsert(this);
+	if((this.maxDepth > 4)&&(this.maxDepth > (this.minDepth * 2))) {
+		this.rebalance();
+	}
+}
+
+BinarySearchTree.prototype.rebalance =  function() {
+	debugger;
+	var list = [];
+	this.depthFirstLog(function(item){list.push(item);});
+	list.sort();
+	var firstIndex = Math.floor(list.length/2);
+	var newTree = BinarySearchTree(list[firstIndex]);
+
+	var recursivePopulate = function(start, end) {
+		if(start >= end) {
+			newTree.insert(list[start]);
+			return;
+		}
+		var newNodeIndex = Math.floor(start + ((end-start)/2));
+		newTree.insert(list[newNodeIndex]);
+		recursivePopulate(start, newNodeIndex-1);
+		recursivePopulate(newNodeIndex+1, end);
+	}
+
+	recursivePopulate(0, Math.floor(list.length / 2));
+	recursivePopulate(Math.floor(list.length / 2), list.length);
+	debugger;
+	this.value = newTree.value;
+	this.left = newTree.left;
+	this.right = newTree.right;
 }
 
 BinarySearchTree.prototype.contains = function(value){	
@@ -80,6 +129,10 @@ BinarySearchTree.prototype.depthFirstLog = function(callback){
 	}
 }
 
+var b = BinarySearchTree(50);
+for(var i = 0; i<100; i++) {
+	b.insert(Math.random()*100);
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
